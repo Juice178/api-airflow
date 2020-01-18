7b75c705a76b61a6b8c0ba2409a57e03d107a96b
Kenta <kentasuzuki5555@gmail.com>
2020-01-19 01:23:16 +0900

    Add a validation method to spotipy class

--- apis/spotify.py
+++ apis/spotify.py
@@ -4,18 +4,52 @@
 from spotipy import Spotify, SpotifyException
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOauthError
 
+
 class Spotipy(object):
     def __init__(self, client_id, client_secret):
-        self._sp = self.get_client(client_id, client_secret)
+        self._sp = self.set_client(client_id, client_secret)
 
-    def get_client(self, client_id, client_secret):
+    def set_client(self, client_id, client_secret):
+        """
+        Creates a Spotify API client
+        """
         client_credentials_manager = SpotifyClientCredentials(client_id, client_secret)
-        return Spotify(client_credentials_manager=client_credentials_manager)
+        sp = Spotify(client_credentials_manager=client_credentials_manager)
 
-    def get_artist_top_tracks(self, artist_id):
+        if not self.is_credential_valid(sp):
+            raise Exception("Invalid credential")
+
+        return sp
+
+    def is_credential_valid(self, sp):
+        """
+        Validate credential
+        """
+        try:
+            sp._auth_headers()
+        except SpotifyOauthError:
+            return False
+        else:
+            return True
+
+    def get_artist_top_10_tracks(self, artist_id, country):
+        """ Get Spotify catalog information about an artist's top 10 tracks
+            by country.
+            Parameters:
+                - artist_id(str) - the artist ID
+                - country(str) - limit the response to one particular country
+            Returns:
+                - Information about 10 tracks (list)
+        """
         artist_uri = f'spotify:artist:{artist_id}'
-        return  self._sp.artist_top_tracks(artist_uri)
+        return  self._sp.artist_top_tracks(artist_uri, country)["tracks"]
 
-    def get_audio_features(self, song_id):
-        return self._sp.audio_features(song_id)
+    def get_audio_features(self, tracks):
+        """
+        Parameters:
+            - tracks(list)
+        Returns:
+            - (list) audio features about each song corresponding to each track
+        """
+        return self._sp.audio_features(tracks)
 
